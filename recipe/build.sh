7aff58393895c7dd4401c5924c0ecea530fdbe6a
Valerio Mariani <valmar@slac.stanford.edu>
2023-02-28 17:59:17 -0800

    Initial version for LCLS-I

---
+++ recipe/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x -e
+
+declare -a CMAKE_PLATFORM_FLAGS
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX}/${HOST}/sysroot")
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/include")
+
+if [[ ${DEBUG_C} == yes ]]; then
+  CMAKE_BUILD_TYPE=Debug
+else
+  CMAKE_BUILD_TYPE=Release
+fi
+
+
+export LDFLAGS="-lrt"
+export PKG_CONFIG_PATH=${CONDA_PREFIX}/share/pkgconfig:${CONDA_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
+
+env |sort
+
+mkdir build
+cd build
+${CONDA_PREFIX}/bin/cmake --version
+${CONDA_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_STATS=ON \
+  -DBUILD_PASTRI=ON \
+  -DBUILD_PYTHON_WRAPPER=OFF \
+  -DBUILD_NETCDF_READER=OFF \
+  -DBUILD_HDF5_FILTER=OFF \
+  "${CMAKE_PLATFORM_FLAGS[@]}" \
+  ${SRC_DIR}
+
+${CONDA_PREFIX}/bin/make -j${CPU_COUNT}
+${CONDA_PREFIX}/bin/make install PREFIX=${PREFIX}
